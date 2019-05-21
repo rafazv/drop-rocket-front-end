eaefa98f28c6ba18d81df8fc91bd614002fe6e56
Rafaela Lima <rafaela.amazonas@gmail.com>
2019-05-21 15:33:45 -0400

    Finish front-end

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { distanceInWords } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import socket from 'socket.io-client';
 
 import Dropzone from 'react-dropzone';
 
@@ -14,11 +15,35 @@ export default class Box extends Component {
     state = { box: {} };
 
     async componentDidMount() {
+		this.subscribeToNewFiles();
+
         const box = this.props.match.params.id;
         const response = await api.get(`boxes/${box}`);
 
         this.setState({ box: response.data });
-    }
+	}
+	
+	subscribeToNewFiles = () => {
+		const box = this.props.match.params.id;
+		const io = socket("https://drop-rocket-backend.herokuapp.com/");
+
+		io.emit("connectRoom", box);
+
+		io.on("file", data => {
+			this.setState({ box: { ...this.state.box, files: [data, ...this.state.box.files]} })
+		});
+	};
+
+	handleUpload = files => {
+		files.forEach(file => {
+			const data = new FormData();
+			const box = this.props.match.params.id;
+
+			data.append('file', file);
+
+			api.post(`boxes/${box}/files`, data);
+		});
+	};
 
   render() {
     return (
@@ -28,7 +53,7 @@ export default class Box extends Component {
                 <h1>{this.state.box.title}</h1>
             </header>
 
-            <Dropzone>
+            <Dropzone onDropAccepted={this.handleUpload}>
                 {({getRootProps, getInputProps}) => (
                     <div className="upload" {...getRootProps()}>
                         <input {...getInputProps()}/>
@@ -39,7 +64,7 @@ export default class Box extends Component {
 
             <ul>
                 { this.state.box.files && this.state.box.files.map(file => (
-                    <li>
+                    <li key={file._id}>
                         <a className="fileInfo" href={file.url} target="blank">
                             <MdInsertDriveFile size={24} color="#A5Cfff" />
                             <strong>{file.title}</strong>
